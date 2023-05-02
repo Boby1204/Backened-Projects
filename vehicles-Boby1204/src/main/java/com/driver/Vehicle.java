43c17ebd9b571cc5bddd34a5d472394a5b1c8254
Boby Kanyal <96154157+Boby1204@users.noreply.github.com>
2023-05-02 16:08:32 +0530

    Add files via upload

---
+++ vehicles-Boby1204/src/main/java/com/driver/Vehicle.java
@@ -0,0 +1,39 @@
+package com.driver;
+
+public class Vehicle {
+
+    private String name;
+    private int currentSpeed;
+    private int currentDirection;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void steer(int direction){
+        this.currentDirection = direction;
+        //direction is in degrees, add it to the current direction
+        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    }
+
+    public void move(int speed, int direction){
+        //set the values of currentSpeed and currentDirection
+        this.currentDirection = direction;
+        this.currentSpeed = speed;
+        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
+    }
+
+    public void stop(){
+        this.currentSpeed = 0;
+        System.out.println("stop method called - The vehicle is stopped");
+    }
+
+}
